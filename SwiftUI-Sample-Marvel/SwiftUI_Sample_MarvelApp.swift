5ba1bee513543d13f4c11ab81ec347209a2e8467
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2022-07-11 17:44:08 -0400

    initial commit

---
+++ SwiftUI-Sample-Marvel/SwiftUI_Sample_MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sample_MarvelApp.swift
+//  SwiftUI-Sample-Marvel
+//
+//  Created by Dylan Perry on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
