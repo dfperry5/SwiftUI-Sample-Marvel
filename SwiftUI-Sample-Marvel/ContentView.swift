d1469a72c870af7c1f92d340d1b9a6b6d393cf9c
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2022-07-14 14:08:05 -0400

    Updating this branch

--- SwiftUI-Sample-Marvel/ContentView.swift
+++ SwiftUI-Sample-Marvel/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
       Text("Hello, world!")
         .font(.title2)
+        .fontWeight(.bold)
+        .foregroundColor(.pink)
         .padding()
     }
 }
